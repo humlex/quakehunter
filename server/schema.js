3ad5565f8c2a3010be54516bf7d23f00b69be573
clairobscour <clairobscour@gmail.com>
2021-07-19 13:36:26 +0300

    (feat): added types to the schema

--- server/schema.js
+++ server/schema.js
@@ -2,9 +2,37 @@ const { gql } = require("apollo-server");
 
 const typeDefs = gql`
   type Query {
-    launches: [Launch]!
-    launch(id: ID!): Launch
+    quakes: [Quake]!
+    quake(id: ID!): Quake
     me: User
+  }
+
+  type Quake {
+    id: ID!
+    location: String
+    magnitude: Float
+    when: String
+    time: String
+  }
+
+  type User {
+    id: ID!
+    username: String!
+    email: String!
+    password: String!
+    records: [Quake]!
+  }
+
+  type Mutation {
+    saveRecord(recordId: ID!): RecordUpdateResponse!
+    deleteRecord(recordId: ID!): RecordUpdateResponse!
+    login(email: String): User
+  }
+
+  type RecordUpdateResponse {
+    success: Boolean!
+    message: String
+    records: [Quake]
   }
 `;
 
