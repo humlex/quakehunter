0dc906bdf66ae6a439a0efa6672d8eeffb38d85f
clairobscour <clairobscour@gmail.com>
2021-07-21 13:09:48 +0300

    (feat): changed quakes resolver

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,6 +1,23 @@
 module.exports = {
   Query: {
-    quakes: (_, __, { dataSources }) => dataSources.quakeAPI.getAllQuakes(),
+    quakes: async (_, { pageSize = 20, after }, { dataSources }) => {
+      const allQuakes = await dataSources.quakesAPI.getAllQuakes();
+      allQuakes.reverse();
+      const quakes = paginateResults({
+        after,
+        pageSize,
+        results: allQuakes,
+      });
+
+      return {
+        quakes,
+        cursor: quakes.length ? quakes[quakes.length - 1].cursor : null,
+        hasMore: quakes.length
+          ? quakes[quakes.length - 1].cursor !==
+            allQuakes[allQuakes.length - 1].cursor
+          : false,
+      };
+    },
     quake: (_, { id }, { dataSources }) =>
       dataSources.quakeAPI.getQuakeById({ quakeId: id }),
     users: (_, __, { dataSources }) => dataSources.userAPI.getUsers(),
